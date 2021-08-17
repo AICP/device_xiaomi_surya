e9860e837c3e3a4312f8a77268741dd0dc23c968
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-17 14:10:24 +0200

    surya: Add doze back
    * Adapt to aicp
    
    This reverts commit 00f295ad8b71eba0bf111505c2933a44d1a3e1d2.
    
    Change-Id: I8bab03902b9343d19ba4474b5253971d4f171713

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
+        DozeUtils.checkDozeService(context);
         HapticUtils.restoreLevel(context);
     }
 }
